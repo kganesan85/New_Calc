4d955c4f71aba4d466151d1970c26ba363d1ba3b
kganesan85 <gkowsalyaa@gmail.com>
2015-06-29 13:41:31 -0700

    Update Calc.php

--- Calc.php
+++ Calc.php
@@ -4,6 +4,67 @@ require_once 'TerminalExpression.php';
 require_once 'Calculations.php';
 
 class Calc {
+    
+     public function evaluate($string) {
+        $stack = $this->parse($string);
+        return $this->run($stack);
+    }
+    
+    public function parse($string) {
+        $tokens = $this->tokenize($string);
+        $output = new Stack();
+        $operators = new Stack();
+        foreach ($tokens as $token) {
+            $token = $this->extractVariables($token);
+            $expression = TerminalExpression::factory($token);
+            if ($expression->isOperator()) {
+                $this->parseOperator($expression, $output, $operators);
+            } elseif ($expression->isParenthesis()) {
+                $this->parseParenthesis($expression, $output, $operators);
+            } else {
+                $output->push($expression);
+            }
+        }
+        while (($op = $operators->pop())) {
+            if ($op->isParenthesis()) {
+                throw new RuntimeException('Mismatched Parenthesis');
+            }
+            $output->push($op);
+        }
+        return $output;
+    }
+
+    public function registerVariable($name, $value) {
+        $this->variables[$name] = $value;
+    }
+
+    public function run(Stack $stack) {
+        while (($operator = $stack->pop()) && $operator->isOperator()) {
+            $value = $operator->operate($stack);
+            if (!is_null($value)) {
+                $stack->push(TerminalExpression::factory($value));
+            }
+        }
+        return $operator ? $operator->render() : $this->render($stack);
+    }
+
+    protected function extractVariables($token) {
+        if ($token[0] == '$') {
+            $key = substr($token, 1);
+            return isset($this->variables[$key]) ? $this->variables[$key] : 0;
+        }
+        return $token;
+    }
+
+    protected function render(Stack $stack) {
+        $output = '';
+        while (($el = $stack->pop())) {
+            $output .= $el->render();
+        }
+        if ($output) {
+            return $output;
+        }
+        throw new RuntimeException('Could not render output');
 
     protected function parseOperator(TerminalExpression $expression, Stack $output, Stack $operators) {
         $end = $operators->poke();
